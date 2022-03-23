63765bc51734a6b5dcfab30c093641fd440ba555
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2022-03-23 18:04:53 +0700

    lab3

---
+++ lab3/index.php
@@ -0,0 +1,54 @@
+<!-- Request của www.thegioididong.com
+    General
+        Request URL: https://www.thegioididong.com/
+        Request Method: GET
+        Status Code: 200 
+        Remote Address: 222.255.236.148:443
+        Referrer Policy: strict-origin-when-cross-origin
+    Request header
+        :authority: www.thegioididong.com
+        :method: GET
+        :path: /
+        :scheme: https
+        accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+        accept-encoding: gzip, deflate, br
+        accept-language: vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5
+        cache-control: max-age=0
+        cookie: ShowLocationSuggest=hide; lhc_per=vid|efbf0941292c943bf574; chat.info=; chat.username=20210803D1i8tV9PKAXa_ITaAEf_; chat.notifychatmsg=; DMX_Personal=%7B%22CustomerId%22%3A0%2C%22CustomerSex%22%3A0%2C%22CustomerName%22%3Anull%2C%22CustomerPhone%22%3Anull%2C%22Address%22%3A%22%22%2C%22CurrentUrl%22%3Anull%2C%22ProvinceId%22%3A129%2C%22ProvinceName%22%3A%22L%C3%A2m%20%C4%90%E1%BB%93ng%22%2C%22DistrictId%22%3A1111%2C%22DistrictType%22%3A%22Th%C3%A0nh%20ph%E1%BB%91%22%2C%22DistrictName%22%3A%22%20%C4%90%C3%A0%20L%E1%BA%A1t%22%2C%22WardId%22%3A5452%2C%22WardType%22%3A%22Ph%C6%B0%E1%BB%9Dng%22%2C%22WardName%22%3A%22%202%22%2C%22StoreId%22%3A0%7D; mwgngxpv=129; tgdd_province=129; SvID=line8084|YjH3n|YjH3n; _ga_TLRZMSX5ME=GS1.1.1647441821.13.0.1647441821.60; _ga=GA1.2.1363485747.1631148762; _gid=GA1.2.149103721.1647441822
+        sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"
+        sec-ch-ua-mobile: ?0
+        sec-ch-ua-platform: "Windows"
+        sec-fetch-dest: document
+        sec-fetch-mode: navigate
+        sec-fetch-site: none
+        sec-fetch-user: ?1
+        upgrade-insecure-requests: 1
+        user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36
+
+
+Request của tuoitre.vn
+    General
+        Request URL: https://tuoitre.vn/
+        Request Method: GET
+        Status Code: 200 
+        Remote Address: 222.255.239.80:443
+        Referrer Policy: strict-origin-when-cross-origin
+    Request header
+        :authority: tuoitre.vn
+        :method: GET
+        :path: /
+        :scheme: https
+        accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+        accept-encoding: gzip, deflate, br
+        accept-language: vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5
+        cache-control: max-age=0
+        cookie: G_ENABLED_IDPS=google; __R=2; __RC=41; _uidcms=1635731658609274818; __stat="BLOCK"; __stp={"visit":"returning","uuid":"2c9f1e24-d9b0-408c-bdc6-497952f53276"}; __UF=-1; __tb=0; __zi=3000.SSZzejyD3CiaW_sbrKeErsE1gRkRH1QKFvEZf8a6188lrFpYYGT1oYZLyU2AJaNABPlsu8vFGCmrCpa.1; _ga_8KQ37P0QJM=GS1.1.1647442652.1.0.1647442652.0; __sts={"sid":1647442653480,"tx":1647442653480,"url":"https%3A%2F%2Ftuoitre.vn%2F","pet":1647442653480,"set":1647442653480}; _ga=GA1.2.879510196.1635731651; _gid=GA1.2.1344764474.1647442654; _gat_UA-46730129-1=1; _gat_tto_vcc=1; __stdf=0; __stbpnenable=1; __stgeo="denied"
+        sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"
+        sec-ch-ua-mobile: ?0
+        sec-ch-ua-platform: "Windows"
+        sec-fetch-dest: document
+        sec-fetch-mode: navigate
+        sec-fetch-site: none
+        sec-fetch-user: ?1
+        upgrade-insecure-requests: 1
+        user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 -->
